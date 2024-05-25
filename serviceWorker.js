b4f9e3dcc867ef9c94abb5accf08129e03e75587
IIMURA Takuji <uirou.j@gmail.com>
2024-05-25 22:31:02 +0900

    タブを開いた時の初期値が OFF になっていた問題を修正します。

--- serviceWorker.js
+++ serviceWorker.js
@@ -77,15 +77,75 @@ function disableJustAmazon(tabId){
   );
 }
 
-function ToggleJustAmazon(tab) {
+// https://developer.chrome.com/docs/extensions/reference/api/offscreen?hl=ja のものをそのまま使います
+let offScreenDocumentCreating; // A global promise to avoid concurrency issues
+async function setupOffscreenDocument(path) {
+  // Check all windows controlled by the service worker to see if one
+  // of them is the offscreen document with the given path
+  const offscreenUrl = chrome.runtime.getURL(path);
+  const existingContexts = await chrome.runtime.getContexts({
+    contextTypes: ['OFFSCREEN_DOCUMENT'],
+    documentUrls: [offscreenUrl]
+  });
+
+  if (existingContexts.length > 0) {
+    return;
+  }
+
+  // create offscreen document
+  if (offScreenDocumentCreating) {
+    await offScreenDocumentCreating;
+  } else {
+    offScreenDocumentCreating = chrome.offscreen.createDocument({
+      url: path,
+      reasons: ['WORKERS'],
+      justification: "個々のタブでの ON/OFF 状態を一時的に保存するために使います。\n具体的には、ブラウザを再起動すると消えるようにするために offscreen 側でタブのON/OFF状態を保存します(以前の background.js の代わりとして用います)。",
+    });
+    await offScreenDocumentCreating;
+    offScreenDocumentCreating = null;
+  }
+}
+
+function sendToStateKeeper(message) {
+  return new Promise((resolve, reject) => {
+    chrome.runtime.sendMessage(message, (response) => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      }else{
+        resolve(response);
+      }
+    });
+  });
+}
+
+let stateKeeperHtmlPath = "tabStateKeeper.html";
+async function saveState(tabId, state) {
+  await setupOffscreenDocument(stateKeeperHtmlPath);
+  await sendToStateKeeper({type: "save", key: tabId, value: state});
+}
+async function loadState(tabId) {
+  await setupOffscreenDocument(stateKeeperHtmlPath);
+  try {
+    let response = await sendToStateKeeper({type: "load", key: tabId});
+    return response;
+  }catch(error){
+    console.log("JACP: load got error:", error);
+  }
+  return undefined;
+}
+
+async function ToggleJustAmazon(tab) {
   var currentURL = tab.url;
-  console.log("ToggleJustAmazon called", currentURL);
   if(!isAmazonQueryURL(currentURL)){
     return;
   }
+  // state としては、「JustAmazonが無効の場合が True」です。
+  // 初期値は undefined で False であるので、初期値が得られた場合に「JustAmazonが有効」と判定させるためです。
   if(isJustAmazonURL(currentURL)){
+    await saveState(tab.id, true);
     disableJustAmazon(tab.id);
   }else{
+    await saveState(tab.id, false);
     enableJustAmazon(tab.id);
   }
   chrome.tabs.reload(tab.Id);
@@ -93,7 +153,7 @@ function ToggleJustAmazon(tab) {
 
 chrome.action.onClicked.addListener(ToggleJustAmazon);
 
-function updateAppIconStatus(tabId) {
+async function updateAppIconStatus(tabId) {
   chrome.tabs.get(tabId, async function(tab){
     var targetURL = tab.url;
     if(!isAmazonQueryURL(targetURL)){
@@ -101,13 +161,21 @@ function updateAppIconStatus(tabId) {
       return;
     }
     enableActionButton(tabId);
-    let isEnabled = isJustAmazonURL(targetURL);
-    if(isEnabled) {
+    let tabState = await loadState(tabId);
+    if(!tabState) { // state としては、「JustAmazonが無効の場合が True」なので、!tabState であれば Enable にする方向で作業します。
       setEnableIcon(tabId);
       enableJustAmazon(tabId);
+      let isEnabled = isJustAmazonURL(targetURL);
+      if(!isEnabled) {
+        chrome.tabs.reload(tab.Id);
+      }
     }else{
       setDisableIcon(tabId);
       disableJustAmazon(tabId);
+      let isEnabled = isJustAmazonURL(targetURL);
+      if(isEnabled) {
+        chrome.tabs.reload(tab.Id);
+      }
     }
   });
 }
