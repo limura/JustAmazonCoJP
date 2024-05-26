6850d5d40e956f06b534d2e016deb9ef409313ad
IIMURA Takuji <uirou.j@gmail.com>
2024-05-26 14:27:38 +0900

    console.log()を削除しておきます。

--- tabStateKeeper.js
+++ tabStateKeeper.js
@@ -1,15 +1,12 @@
 var state = {};
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    //console.log("onMessage", request, request.type, request.key, request.value);
     switch (request.type) {
         case "save":
-            console.log("save[", request.key, "] = ", request.value);
             state[request.key] = request.value;
             sendResponse(request.value);
             return;
         case "load":
-            //console.log("load[", request.key, "] -> ", state[request.key]);
             sendResponse(state[request.key]);
             return;
         default:
@@ -17,4 +14,3 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     }
     sendResponse(undefined);
 });
-  
